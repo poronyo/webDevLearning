88e24ea7c6ba8f8895852ee23cfbe35b3de150e2
pae <parinyat@syssteel.com>
2024-11-06 22:00:37 +0700

    npm learning

--- 2.3_NPM/index.js
+++ 2.3_NPM/index.js
@@ -1,4 +1,51 @@
-import testing from "sillyname";
+// import testing from "superheroes"  assert { type: 'json' };
 
-let testname = testing();
-console.log(`my name is ${testname}`) 
+
+//  let heroName = testing.random()
+
+// console.log(`my name is ${heroName}`) 
+
+// import superheroes from 'superheroes';
+
+// ---------------------
+
+// import testingFunction from "sillyname";
+// 
+// let randomName = testingFunction();
+// 
+// console.log(`My name is ${randomName}`)
+
+// ---------------------
+
+// import superheroes from "superheroes";
+// import testingFunction from "superheroes" ;
+// 
+// const randomSuperhero = superheroes.random();
+// 
+// console.log(`My name is ${randomSuperhero}`);
+// 
+// ---------------------
+
+
+import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from "fs"
+inquirer
+  .prompt([{
+    message : "Type in your URL:",
+    name: "URL"}
+    /* Pass your questions in here */
+  ])
+  .then((answers) => {
+    const url = answer.URL;
+    var qr_svg = qr.image(URL);
+    qr_svg.pipe(require('fs').createWriteStream('qr-image_png'));
+    // Use user feedback for... whatever!!
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
