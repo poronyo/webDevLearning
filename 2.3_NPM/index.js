5993f4d2c4321dbf3317977f79ac5bdefad9ceac
Parinya Tosuwan <parinyat@syssteel.com>
2024-11-11 22:30:45 +0700

    learning_npm

--- 2.3_NPM/index.js
+++ 2.3_NPM/index.js
@@ -1,46 +1,53 @@
 // import testing from "superheroes"  assert { type: 'json' };
 
-
 //  let heroName = testing.random()
 
-// console.log(`my name is ${heroName}`) 
+// console.log(`my name is ${heroName}`)
 
 // import superheroes from 'superheroes';
 
 // ---------------------
 
 // import testingFunction from "sillyname";
-// 
+//
 // let randomName = testingFunction();
-// 
+//
 // console.log(`My name is ${randomName}`)
 
 // ---------------------
 
 // import superheroes from "superheroes";
 // import testingFunction from "superheroes" ;
-// 
+//
 // const randomSuperhero = superheroes.random();
-// 
+//
 // console.log(`My name is ${randomSuperhero}`);
-// 
+//
 // ---------------------
-
 
 import inquirer from "inquirer";
 import qr from "qr-image";
-import fs from "fs"
+import fs from "fs";
+
 inquirer
-  .prompt([{
-    message : "Type in your URL:",
-    name: "URL"}
-    /* Pass your questions in here */
+  .prompt([
+    {
+      name: "URL",
+      message: "Type in your URL:",
+    },
   ])
   .then((answers) => {
-    const url = answer.URL;
-    var qr_svg = qr.image(URL);
-    qr_svg.pipe(require('fs').createWriteStream('qr-image_png'));
-    // Use user feedback for... whatever!!
+    console.log(answers);
+    const url = answers.URL;
+
+    let qr_svg = qr.image(url, { size: 10 });
+    qr_svg.pipe(fs.createWriteStream("my_qrImage.png"));
+    console.log(qr_svg);
+
+    fs.writeFile("URL_Message.txt", url, (err) => {
+      if (err) throw err;
+      console.log("The file has been saved!! ");
+    });
   })
   .catch((error) => {
     if (error.isTtyError) {
