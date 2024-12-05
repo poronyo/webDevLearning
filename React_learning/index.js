af1e51ed711277a0f8619dfbed24e70ac9de07a6
pae <parinyat@syssteel.com>
2024-12-05 21:08:17 +0700

    keeper app with map function

--- React_learning/index.js
+++ React_learning/index.js
@@ -45,32 +45,78 @@ var numbers = [3, 56, 2, 48, 5];
 // })
 // 
 // console.log(newIndexNumber)
-const emojipedia = [
-    {
-      id: 1,
-      emoji: "💪",
-      name: "Tense Biceps",
-      meaning:
-        "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
-    },
-    {
-      id: 2,
-      emoji: "🙏",
-      name: "Person With Folded Hands",
-      meaning:
-        "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
-    },
-    {
-      id: 3,
-      emoji: "🤣",
-      name: "Rolling On The Floor, Laughing",
-      meaning:
-        "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
-    }
-  ];
-const meaningText = emojipedia.map(function(array){
+// const emojipedia = [
+//     {
+//       id: 1,
+//       emoji: "💪",
+//       name: "Tense Biceps",
+//       meaning:
+//         "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+//     },
+//     {
+//       id: 2,
+//       emoji: "🙏",
+//       name: "Person With Folded Hands",
+//       meaning:
+//         "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+//     },
+//     {
+//       id: 3,
+//       emoji: "🤣",
+//       name: "Rolling On The Floor, Laughing",
+//       meaning:
+//         "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+//     }
+//   ];
+// const meaningText = emojipedia.map(function(array){
     
-    return array.meaning.substring(0,100)
+//     return array.meaning.substring(0,100)
+// })
+
+// console.log(meaningText)
+//--------------------------------------------------
+
+
+
+
+//--------------------------------------------------
+
+// 
+//Map -Create a new array by doing something with each item in an array.
+const newNumbers = numbers.map( x => x*2);
+
+////Filter - Create a new array by keeping the items that return true.
+// const newNumbers = numbers.filter(function(num) {
+  // return num < 10;
+// });
+// 
+const newFilterNumber = numbers.filter(num => num<10)
+// 
+// Reduce - Accumulate a value by doing something to each item in an array.
+// var newNumber = numbers.reduce(function (accumulator, currentNumber) {
+    // return accumulator + currentNumber;
+// })
+
+var newAccumulateNumber = numbers.reduce((accumulator, currentNumber) => accumulator+currentNumber)
+
+//Find - find the first item that matches from an array.
+// const newNumber = numbers.find(function (num) {
+  // return num > 10;
+// })
+
+const findNewNumber = numbers.find(num => num>10);
+
+
+// FindIndex - find the index of the first item that matches.
+const newNumber = numbers.findIndex(function (num) {
+  return num > 10;
 })
 
-console.log(meaningText)
+const newIndexNumber = numbers.findIndex(num => num>10);
+
+console.log("numbers",numbers)
+console.log("new unmber : ",newNumbers)
+console.log("new filter unmber : ",newFilterNumber)
+console.log("accumelate : ",newAccumulateNumber)
+console.log("findNewNumber :",findNewNumber)
+console.log("newIndexNumber :",newIndexNumber)
