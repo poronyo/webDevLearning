cef6aa7d97e83bb058daba422015aae75bedbe71
pae <parinyat@syssteel.com>
2024-12-08 21:42:05 +0700

    test get local string time

--- React_learning/index.js
+++ React_learning/index.js
@@ -83,40 +83,55 @@ var numbers = [3, 56, 2, 48, 5];
 
 // 
 //Map -Create a new array by doing something with each item in an array.
-const newNumbers = numbers.map( x => x*2);
+// const newNumbers = numbers.map( x => x*2);
 
 ////Filter - Create a new array by keeping the items that return true.
 // const newNumbers = numbers.filter(function(num) {
   // return num < 10;
 // });
 // 
-const newFilterNumber = numbers.filter(num => num<10)
+// const newFilterNumber = numbers.filter(num => num<10)
 // 
 // Reduce - Accumulate a value by doing something to each item in an array.
 // var newNumber = numbers.reduce(function (accumulator, currentNumber) {
     // return accumulator + currentNumber;
 // })
 
-var newAccumulateNumber = numbers.reduce((accumulator, currentNumber) => accumulator+currentNumber)
+// var newAccumulateNumber = numbers.reduce((accumulator, currentNumber) => accumulator+currentNumber)
 
 //Find - find the first item that matches from an array.
 // const newNumber = numbers.find(function (num) {
   // return num > 10;
 // })
 
-const findNewNumber = numbers.find(num => num>10);
+// const findNewNumber = numbers.find(num => num>10);
 
 
 // FindIndex - find the index of the first item that matches.
-const newNumber = numbers.findIndex(function (num) {
-  return num > 10;
-})
+// const newNumber = numbers.findIndex(function (num) {
+  // return num > 10;
+// })
 
-const newIndexNumber = numbers.findIndex(num => num>10);
+// const newIndexNumber = numbers.findIndex(num => num>10);
 
-console.log("numbers",numbers)
-console.log("new unmber : ",newNumbers)
-console.log("new filter unmber : ",newFilterNumber)
-console.log("accumelate : ",newAccumulateNumber)
-console.log("findNewNumber :",findNewNumber)
-console.log("newIndexNumber :",newIndexNumber)
+// console.log("numbers",numbers)
+// console.log("new unmber : ",newNumbers)
+// console.log("new filter unmber : ",newFilterNumber)
+// console.log("accumelate : ",newAccumulateNumber)
+// console.log("findNewNumber :",findNewNumber)
+// console.log("newIndexNumber :",newIndexNumber)
+//--------------------------------------------------
+
+
+//--------------------------------------------------
+
+let time = new Date().toLocaleTimeString();
+console.log(time);
+
+
+function getTime(){
+
+} 
+
+let times = () =>new Date().toLocaleTimeString();
+console.log(times())
